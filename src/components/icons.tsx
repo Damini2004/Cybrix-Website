94191c989e87da77257a460f8197b1311f895b87
Divya Chachane <divyachachane06@gmail.com>
2025-10-06 08:14:48 +0000

    updated

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,7 +8,7 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <div className={props.className}>
       <Image
-        src="/Cybrix.png"
+        src="/Cybrix.jpeg"
         alt="Cybrix Logo"
         width={256} // Original width of the image for aspect ratio
         height={256} // Original height of the image for aspect ratio
