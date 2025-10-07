77bc4367db15f38a878af32556d6cf1a6e68f7c5
Divya Chachane <divyachachane06@gmail.com>
2025-10-07 12:40:06 +0000

    change styling (_for element div_)

--- src/app/(public)/conference/about-conference/page.tsx
+++ src/app/(public)/conference/about-conference/page.tsx
@@ -1,7 +1,7 @@
 
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
-import { CheckCircle, Presentation, BookOpen, Users, Award } from "lucide-react";
+import { CheckCircle, Presentation, Users, Award } from "lucide-react";
 import Image from "next/image";
 
 const corePrinciples = [
