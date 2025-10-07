1c38ba157a88ec3cb9454b3141c2509f0c554331
Divya Chachane <divyachachane06@gmail.com>
2025-10-07 12:13:51 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,23 +4,16 @@ import * as React from "react";
 import Footer from "@/components/layout/footer";
 import UserHeader from "@/components/layout/user-header";
 import WhatsappFab from "@/components/layout/whatsapp-fab";
-import { cn } from "@/lib/utils";
 
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isMounted, setIsMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <div className="flex min-h-screen flex-col">
       <UserHeader />
-      <main className={cn("flex-1", isMounted && "pt-28")}>
+      <main className="flex-1 pt-28">
         {children}
       </main>
       <Footer />
